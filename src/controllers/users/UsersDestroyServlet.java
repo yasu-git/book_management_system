df3d2b3e69b7a8cdd3a570dda5119ba7b5e3d688
yasu <yasu.git.33@gmail.com>
2020-01-05 13:47:14 +0900

    Add user destroy

--- src/controllers/users/UsersDestroyServlet.java
+++ src/controllers/users/UsersDestroyServlet.java
@@ -1,19 +1,24 @@
 package controllers.users;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.User;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class UsersDestroyServlet
  */
 @WebServlet("/users/destroy")
 public class UsersDestroyServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,11 +27,32 @@ public class UsersDestroyServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        String _token =(String)request.getParameter("_token");
+
+        if (_token != null && _token.equals(request.getSession().getId())){
+            EntityManager em = DBUtil.createEntityManager();
+
+            User u = em.find(User.class, (Integer)(request.getSession().getAttribute("user_id")));
+
+            //ユーザーの削除処理
+            em.getTransaction().begin();
+            //ユーザー情報を入れると消せる
+            em.remove(u);
+            em.getTransaction().commit();
+            em.close();
+
+            request.getSession().setAttribute("flush", "削除が完了しました。");
+            request.getSession().removeAttribute("user_id");
+
+            response.sendRedirect(request.getContextPath() + "/users/index");
+
+
+        }
+    }
 
 }
